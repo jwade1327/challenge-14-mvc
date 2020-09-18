797222e62824a3d2389cbe1d188a262263473e31
unknown <jwade1327@gmail.com>
2020-09-18 15:18:09 -0500

    javascript

---
+++ public/javascript/signup.js
@@ -0,0 +1,28 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (email && username && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        }
+        else {
+            alert(response.statusText)
+        }
+    }
+};
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

40eaf2f6163785fe31c9bec26421930a3adaef5d
unknown <jwade1327@gmail.com>
2020-09-18 10:04:29 -0500

    models

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const routes = require('./controllers');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlbars', hbs.engine);
